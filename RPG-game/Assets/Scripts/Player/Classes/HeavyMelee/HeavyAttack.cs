d767334eb5bee7255532baa2c02db7af0201cd9a
Irimia-Sorici Andrei <andreisorici2007@gmail.com>
2023-12-29 10:34:46 +0200

    particule care indica stadiul la heavy attack

--- RPG-game/Assets/Scripts/Player/Classes/HeavyMelee/HeavyAttack.cs
+++ RPG-game/Assets/Scripts/Player/Classes/HeavyMelee/HeavyAttack.cs
@@ -16,9 +16,14 @@ public class HeavyAttack : MonoBehaviour
     public bool canAttack = true;
 
     public static bool heavy;
+    public GameObject heavyAttackParticle;
+    ParticleSystem heavyAttackPs;
+    bool heavyAttackColorChange=true;
+    Coroutine heavyAttackParticleCoroutine;
 
     void Awake()
     {
+        heavyAttackPs = heavyAttackParticle.GetComponent<ParticleSystem>();
         startTimer = 0;
         endTimer = 0;
     }
@@ -40,12 +45,43 @@ public class HeavyAttack : MonoBehaviour
 
         if(Input.GetMouseButtonDown(1))
         {
+            heavyAttackParticle.SetActive(true);
+            //resetam emisia
+            var emission = heavyAttackPs.emission;
+            emission.rateOverTime = 1;
+            //resetam cronometrul
             startTimer = Time.time;
+            //incepem coroutina de schimbat culoarea dupa "timeToHeavyAttack" seconds
+            StopCoroutine(ParticleColorSet());
+            heavyAttackParticleCoroutine = StartCoroutine(ParticleColorSet());
+            heavyAttackColorChange=true;
+        }
+
+        if(Input.GetMouseButton(1))
+        {
+            if(endTimer - startTimer <= timeToHeavyAttack && heavyAttackPs.emission.rateOverTime.constant <= 1000) 
+            {
+                var emission = heavyAttackPs.emission;
+                emission.rateOverTime = new ParticleSystem.MinMaxCurve(1, emission.rateOverTime.constant + 5);
+            }
         }
 
         if(Input.GetMouseButtonUp(1))
         {
             endTimer = Time.time;
+            if((endTimer - startTimer) <= timeToHeavyAttack)
+            {
+                //opreste coroutina care seteaza culoarea 
+                heavyAttackColorChange=false;
+                StopCoroutine(ParticleColorSet());
+                //reseteaza culoarea
+                var main = heavyAttackPs.main;
+                main.startColor = new Color(255, 255, 255, 255);
+                //reseteaza emisia
+                var emission = heavyAttackPs.emission;
+                emission.rateOverTime = 1;
+                heavyAttackParticle.SetActive(false);
+            }
         }
 
         if((endTimer - startTimer) > timeToHeavyAttack && canAttack)
@@ -53,6 +89,7 @@ public class HeavyAttack : MonoBehaviour
             Heavy();
             startTimer = 0;
             endTimer = 0;
+            if(heavyAttackParticleCoroutine == null) heavyAttackColorChange=true;
         }
     }
 
@@ -65,5 +102,22 @@ public class HeavyAttack : MonoBehaviour
     {
         Debug.Log("Heavy attack");
         ShakeCamera.shake = true;
+        //reseteaza culoarea
+        var main = heavyAttackPs.main;
+        main.startColor = new Color(255, 255, 255, 255);
+        //reseteaza emisia
+        var emission = heavyAttackPs.emission;
+        emission.rateOverTime = 1;
+        heavyAttackParticle.SetActive(false);
+    }
+
+    IEnumerator ParticleColorSet()
+    {
+        yield return new WaitForSeconds(timeToHeavyAttack);
+        if(heavyAttackColorChange)
+        {
+            var main = heavyAttackPs.main;
+            main.startColor = new Color(135, 0, 115, 255);
+        }    
     }
 }
