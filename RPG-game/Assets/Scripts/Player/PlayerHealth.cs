3164cf8f183f9be3d2d8af3f46f5ba2081df3717
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2024-01-01 16:00:09 +0200

    facut viata bara

--- RPG-game/Assets/Scripts/Player/PlayerHealth.cs
+++ RPG-game/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float maxHealth = 20;
     public float currentHealth;
-
+    GameObject sliderGO, fillGO, heartGO;
+    Slider slider;
+    public Gradient grad;
+    Image fill, heart;
+    public Sprite[] Images;
     void Start()
     {
+        sliderGO = GameObject.Find("MainCanvas/healthBar");
+        fillGO = GameObject.Find("MainCanvas/healthBar/Fill Area/Fill");
+        heartGO = GameObject.Find("MainCanvas/healthBar/heart");
+        slider = sliderGO.GetComponent<Slider>();
+        fill = fillGO.GetComponent<Image>();
+        heart = heartGO.GetComponent<Image>();
         currentHealth = maxHealth;
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+        heart.sprite = Images[0];
     }
 
     public void Heal(float healAmmount)
@@ -28,5 +42,25 @@ public class PlayerHealth : MonoBehaviour
     void Die()
     {
         Debug.Log("Player is dead");
+    }
+    void Update()
+    {
+        slider.value = currentHealth;
+        fill.color = grad.Evaluate(slider.normalizedValue);
+
+        if(currentHealth/maxHealth < 66.6/100 && currentHealth/maxHealth > 33.3/100)
+        {
+            heart.sprite = Images[1];
+        }
+        else
+        if(currentHealth/maxHealth < 33.3/100)
+        {
+            heart.sprite = Images[2];
+        }
+
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(4);
+        }
     }
 }
