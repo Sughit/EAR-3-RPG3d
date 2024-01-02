3cb0349d43e0a41599620e98eab3e5737b10e0b6
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2024-01-02 18:15:28 +0200

    .

--- RPG-game/Assets/Scripts/GameManager/MainMenuManager.cs
+++ RPG-game/Assets/Scripts/GameManager/MainMenuManager.cs
@@ -7,6 +7,9 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
     public Animator trans;
+    public GameObject main;
+    public GameObject settingsMenu;
+
 
     public void StartGame()
     {
@@ -25,4 +28,15 @@ public class MainMenuManager : MonoBehaviour
         Debug.Log("Quit");
         Application.Quit();
     }
+    public void Settings()
+    {
+        settingsMenu.SetActive(true);
+        main.SetActive(false);
+    }
+    public void BackSettings()
+    {
+        main.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+    
 }
